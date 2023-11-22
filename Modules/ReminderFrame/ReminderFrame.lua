9f3d54f5cc54f3432a6c6bd2608c2e588503301c
Genju <derfloh205@gmail.com>
2023-11-22 14:49:43 +0100

    Consider Starter Build..

--- Modules/ReminderFrame/ReminderFrame.lua
+++ Modules/ReminderFrame/ReminderFrame.lua
@@ -4,7 +4,13 @@ TalentLoadoutReminder.REMINDER_FRAME = {}
 
 function TalentLoadoutReminder.REMINDER_FRAME:UpdateLoadButtonMacro(SET_TO_LOAD)
     local reminderFrame = TalentLoadoutReminder.GGUI:GetFrame(TalentLoadoutReminder.MAIN.FRAMES, TalentLoadoutReminder.CONST.FRAMES.REMINDER_FRAME)
-    local macroText = "/lon " .. SET_TO_LOAD
+    local macroText = ""
+    if SET_TO_LOAD == TalentLoadoutReminder.CONST.STARTER_BUILD then
+        -- care for the snowflake..
+        macroText = "/run C_ClassTalents.SetStarterBuildActive(true)"
+    else
+        macroText = "/lon " .. SET_TO_LOAD
+    end
 
     ---@type GGUI.Button
 	local loadSetButton = reminderFrame.content.loadButton
