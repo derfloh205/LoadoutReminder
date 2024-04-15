39471cc279e69a36c8f33c2cbb6fc3b906267cef
Genju <derfloh205@gmail.com>
2024-04-15 06:57:49 +0200

    use ggui 2.1

--- Init/GLibs.lua
+++ Init/GLibs.lua
@@ -1,8 +1,8 @@
 ---@class LoadoutReminder
 local LoadoutReminder = select(2, ...)
 
----@type GGUI-2.0
-LoadoutReminder.GGUI = LibStub:GetLibrary("GGUI-2.0")
+---@type GGUI-2.1
+LoadoutReminder.GGUI = LibStub:GetLibrary("GGUI-2.1")
 
 ---@type GUTIL-2.0
 LoadoutReminder.GUTIL = LibStub:GetLibrary("GUTIL-2.0")
